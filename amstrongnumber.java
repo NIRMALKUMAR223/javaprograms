37a5c804eb33b69ee27ebb41ae9420c81ba1a3fe
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-06-25 23:04:01 +0530

    amstrongnumber

---
+++ amstrongnumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class amstrongnumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int number , last, cal =0,orig; 
+        number=sc.nextInt();
+        orig=number;
+        while (number>0)
+        {
+            last=number%10;
+            cal= cal + last*last*last;
+            number/=10;
+        }
+        if(cal==orig)
+        {
+            System.out.println("amstrong");
+        }
+        else
+        {
+            System.out.println("not an amstrong");
+        }
+    }
+}
