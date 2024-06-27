3dca926cf39edffe42206e8c905aa6566843dcbb
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-06-27 22:49:17 +0530

    perfectnumber

---
+++ perfectnumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class perfectnumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int count=0;
+        for (int i=1;i<=number/2;i++)
+        {
+            if (number%i==0)
+            {
+                count+=i;
+            }
+
+        }
+        if (count==number)
+        {
+            System.out.println("Perfect number");
+        }
+        else
+        {
+            System.out.println("Not A Perfect number");
+        }
+    }
+}
