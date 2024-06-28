bbdb5c42f2e25b24be6745db254116a405af747b
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-06-28 22:26:22 +0530

    update

---
+++ Fpattern.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Fpattern {
+    
+    static void fPattern(int number)
+    {
+        for (int i=0;i<number;i++)
+        {
+            for (int j=0;j<number;j++)
+            {
+                if (i==0 || i==number/2 || j==0)
+                {
+                    System.out.print("*"+" ");
+                }
+                else
+                {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        fPattern(number);
+        sc.close();
+    }
+}
