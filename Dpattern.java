5521578bcc3b4c8a6ba2ca3211233c21c9ea8672
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-06-28 22:22:03 +0530

    d

---
+++ Dpattern.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Dpattern {
+    
+    static void dPattern(int number)
+    {
+        for (int i=0;i<number;i++)
+        {
+            for (int j=0;j<number;j++)
+            {
+                if ((i==0 && j!=number-1) || j==0 || (i==number-1 && j!=number-1) || (j==number-1 && i!=0 && i!=number-1))
+                {
+                    System.out.print("*"+" ");
+                }
+                else
+                {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        dPattern(number);
+        sc.close();
+    }
+}
