8928f962fdff08e92b5b4d8b5a1687516ce753fb
NIRMALKUMAR223 <rockerrock223@gmail.com>
2024-06-29 17:25:46 +0530

    wordcount

---
+++ wordcount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class wordcount
+{
+    public static void main(String[] args)
+    {
+        String str="father of java is james gosling";
+        char[] ch=str.toCharArray();
+        int wordcount=1;
+        for  (char c: ch)
+        {
+            if (c==' ')
+            {
+                wordcount++;
+            }
+        }
+        System.out.println(wordcount);
+    }
+}
